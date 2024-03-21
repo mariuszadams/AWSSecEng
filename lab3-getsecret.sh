e715b12ef5baa8e9b9a9c2c2f1c618e86b009083
mariuszadams <37147820+mariuszadams@users.noreply.github.com>
2024-03-21 22:40:17 +0100

    script update

--- lab3-getsecret.sh
+++ lab3-getsecret.sh
@@ -1,3 +1,12 @@
+### Retrieve an RDS password in AWS Secrets Manager
+### parameter: secret name
+
+## The script defines a shell function named getsecretvalue() that does the following:
+## - Uses the AWS CLI secretsmanager get-secret-value command to retrieve the value of the AWS Secrets Manager secret that is identified by a script input.
+## - Uses the jq utility to isolate the JSON-formatted text inside of the SecretString property of the secret.
+## Uses the jq utility to further parse the username, password, endpoint, and port values from the SecretString property and stores each in its own variable.
+## Runs the mysql command with the appropriate flags, using the variables defined, to connect to the Amazon RDS database.
+
 getsecretvalue() {
   aws secretsmanager get-secret-value --secret-id $1 | \
     jq .SecretString | \
